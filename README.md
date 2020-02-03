ef7527eb7acc716b2deb56c62757419c33e84acc
anthonyslatermusic <anthony.slater111@gmail.com>
2020-02-03 10:51:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-slider-ex-at1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-slider-ex-at1)
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './app/material-module';
+
+// this is the export class from the app's typescript file
+import {SliderComponent} from './slider-component/slider-component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [SliderComponent],
+  declarations: [SliderComponent],
+  bootstrap: [SliderComponent],
+  providers: []
+})
+export class AppModule {}
+
+
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
